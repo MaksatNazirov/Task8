e7a3b5339554bc62a4d33f629dfe1cba109f3971
Maks <nazirov.maksat96@gmail.com>
2019-10-23 15:42:39 +0600

    task8

---
+++ Task8.py
@@ -0,0 +1,34 @@
+
+# def choice_to_number(choice):
+#   return {'Usain': 1, 'Me': 2, 'Aybek': 3} [choice] 
+# """Convert choice to number."""
+# # If choice is Usain you should get back 1.
+# def number_to_choice(number):
+#   place = {1: 'Usain', 2: 'Me', 3: 'Aybek'}
+#   return place[number]
+# """Convert number to choice."""
+# # if number is 1 then return usain bolt.
+# #DO NOT remove lines below here,
+# #this is designed to test your code.
+# def test_choice_to_number():
+#   assert choice_to_number('Usain') == 1
+#   assert choice_to_number('Me') == 2
+#   assert choice_to_number('Aybek') == 3
+#   # print('first working')
+
+# def test_number_to_choice():
+#   assert number_to_choice(1) == 'Usain'
+#   assert number_to_choice(2) == 'Me'
+#   assert number_to_choice(3) == 'Aybek'
+ 
+
+# def test_all():
+#   try:
+#     test_choice_to_number()
+#     test_number_to_choice()
+#   except AssertionError:
+#     print('fail')
+#   else:
+#     print('success')
+# # test your code by un-commenting the line(s) below
+# test_all()
